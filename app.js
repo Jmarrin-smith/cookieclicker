80d2e53a5bcc51c593a62fe71d8304b6d1b59da3
Jmarrin-smith <marrinsmithj@gmail.com>
2024-07-19 11:08:09 +0100

    commit 8
    added design features to improve ux

--- app.js
+++ app.js
@@ -47,46 +47,79 @@ function getCount() {
 getCount();
 //localStorage.clear();
 
+const uimg1 = document.getElementById("uimg1");
+const uimg2 = document.getElementById("uimg2");
+const uimg3 = document.getElementById("uimg3");
+const uimg4 = document.getElementById("uimg4");
+const uimg5 = document.getElementById("uimg5");
+const uimg6 = document.getElementById("uimg6");
+const uimg7 = document.getElementById("uimg7");
+const uimg8 = document.getElementById("uimg8");
+const uimg9 = document.getElementById("uimg9");
+
 function affordable() {
   if (afford(30)) {
     document.getElementById("gran").disabled = false;
+    uimg1.src = "./assets/cookiebtn.png";
   } else {
     document.getElementById("gran").disabled = true;
+    uimg1.src = "./assets/redX.png";
   }
   if (afford(300)) {
     document.getElementById("shop").disabled = false;
+    uimg2.src = "./assets/cookiebtn.png";
   } else {
     document.getElementById("shop").disabled = true;
+    uimg2.src = "./assets/redX.png";
   }
   if (afford(3000)) {
     document.getElementById("factory").disabled = false;
+    uimg3.src = "./assets/cookiebtn.png";
   } else {
     document.getElementById("factory").disabled = true;
+    uimg3.src = "./assets/redX.png";
   }
   if (afford(30000)) {
     document.getElementById("airline").disabled = false;
+    uimg4.src = "./assets/cookiebtn.png";
   } else {
     document.getElementById("airline").disabled = true;
+    uimg4.src = "./assets/redX.png";
   }
   if (afford(300000)) {
     document.getElementById("Bank").disabled = false;
+    uimg5.src = "./assets/cookiebtn.png";
   } else {
     document.getElementById("Bank").disabled = true;
+    uimg5.src = "./assets/redX.png";
   }
   if (afford(100)) {
     document.getElementById("power").disabled = false;
+    uimg6.src = "./assets/cookiebtn.png";
   } else {
     document.getElementById("power").disabled = true;
+    uimg6.src = "./assets/redX.png";
   }
   if (afford(1000)) {
     document.getElementById("mega").disabled = false;
+    uimg7.src = "./assets/cookiebtn.png";
   } else {
     document.getElementById("mega").disabled = true;
+    uimg7.src = "./assets/redX.png";
   }
   if (afford(10000)) {
     document.getElementById("super").disabled = false;
+    uimg8.src = "./assets/cookiebtn.png";
   } else {
     document.getElementById("super").disabled = true;
+    uimg8.src = "./assets/redX.png";
+  }
+  if (afford(500)) {
+    document.getElementById("boost").disabled = false;
+    uimg9.src = "./assets/cookiebtn.png";
+  } else {
+    document.getElementById("boost").disabled = true;
+    uimg9.src = "./assets/redX.png";
   }
 }
 
@@ -148,3 +181,21 @@ mega.addEventListener("click", function () {
 superc.addEventListener("click", function () {
   buy1(10000, 100);
 });
+
+boost = document.getElementById("boost");
+
+function buyboost() {
+  btnp *= 1.5;
+  cps *= 1.5;
+  affordable();
+  Dcps.textContent = cps;
+  setTimeout(function () {
+    btnp /= 3;
+    btmp *= 2;
+    cps /= 3;
+    cps *= 2;
+    Dcps.textContent = cps;
+  }, 30 * 1000);
+}
+
+boost.addEventListener("click", buyboost);
